cc277f86bcd68bba1abd24a8b5ebb7e3c7f2f8cd
Serikzhan0 <maxamatxanovvserikzhan@gmail.com>
2025-12-22 23:33:30 +0500

    done

---
+++ 1_modul/lesson_11/Employee.cs
@@ -0,0 +1,12 @@
+namespace lesson_11;
+
+public class Employee
+
+{
+    public Guid GuidId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    public decimal Salary { get; set; }
+    public int Age { get; set; }
+}
---
+++ 1_modul/lesson_11/School.cs
@@ -0,0 +1,14 @@
+namespace lesson_11;
+
+public class School
+{
+    public int Number { get; set; }
+    public string? Name { get; set; }
+    public string Location { get; set; }
+    public int Capacity { get; set; }
+    public int CurrentStudentCount { get; set; }
+}
+    
+     
+    
+    
