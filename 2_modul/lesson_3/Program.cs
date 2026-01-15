63ea1761388e37f32119f5ad68e9f3df3faa6fa4
Serikzhan0 <maxamatxanovvserikzhan@gmail.com>
2026-01-15 17:15:31 +0500

    done

---
+++ 2_modul/lesson_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using lesson_3.Models;
+
+namespace lesson_3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Car car = new Car("White", "Toyota kamri", 15999, "Toyota");
+        new Car();
+        Console.WriteLine(car.Color);
+        car.Foo();
+
+
+        //Car car1 = new Car()
+        //{
+        //    Color = "Red",
+        //    Brand = "GM",
+        //    Model = "Damas",
+        //    Price = 8999
+        //};
+
+        //Car car2 = new Car();
+        //car2.Brand = "BMW";
+        //car2.Price = 25699;
+        //car2.Model = "X5";
+        //car2.Color = "Black";
+
+        //Car car3 = new Car("White", "Toyota kamri", 15999, "Toyota");
+
+
+    }
+}
---
+++ 2_modul/lesson_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using lesson_4.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Teacher teacher = new Teacher("Ali");
+        teacher.Teach();
+
+        Student student = new Student("Bek");
+        student.Study();
+    }
+}
